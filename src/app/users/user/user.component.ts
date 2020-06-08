2f11571632d72a5d744d00241180b0e3d3515e23
GERALD Jr <moswaldgerald@gmail.com>
2020-06-08 18:09:26 +0300

    observables

--- src/app/users/user/user.component.ts
+++ src/app/users/user/user.component.ts
@@ -1,5 +1,5 @@
-import { Component, OnInit } from '@angular/core';
-import {ActivatedRoute} from "@angular/router";
+import {Component, OnInit} from '@angular/core';
+import {ActivatedRoute, Params} from "@angular/router";
 
 @Component({
   selector: 'app-user',
@@ -7,15 +7,23 @@ import {ActivatedRoute} from "@angular/router";
   styleUrls: ['./user.component.css']
 })
 export class UserComponent implements OnInit {
-  user: {id: number, name: string};
+  user: { id: number, name: string };
 
-  constructor(private route: ActivatedRoute) { }
+  constructor(private route: ActivatedRoute) {
+  }
 
   ngOnInit() {
     this.user = {
-      id:this.route.snapshot.params['id'],
-      name:this.route.snapshot.params['name']
+      id: this.route.snapshot.params['id'],
+      name: this.route.snapshot.params['name']
     }
+    this.route.params
+      .subscribe(
+        (params: Params) => {
+          this.user.id = params['id'];
+          this.user.name = params['name'];
+        }
+      );
   }
 
 }
