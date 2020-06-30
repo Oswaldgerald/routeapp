7812281a91e68672971da85b2aabb4cdabd1979a
GERALD Jr <moswaldgerald@gmail.com>
2020-06-30 22:19:43 +0300

    routing

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -16,10 +16,12 @@ import {RouterModule, Routes} from "@angular/router";
 const appRoutes: Routes = [
   {path: '', component: HomeComponent},
   {path: 'users', component: UsersComponent},
-  {path: 'users/:id/:name', component: UserComponent},
-  {path: 'servers', component: ServersComponent},
-  {path: 'servers/:id', component: ServersComponent},
-  {path: 'servers/:id/edit', component: EditServerComponent},
+  {path: 'users/:id/:name', component: UsersComponent},
+  {path: 'servers', component: ServersComponent, children:[
+      {path: ':id', component: ServerComponent},
+      {path: ':id/edit', component: EditServerComponent},
+  ]},
+
 ];
 
 @NgModule({
