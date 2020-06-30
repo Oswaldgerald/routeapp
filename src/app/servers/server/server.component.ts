33ce9ce844c29b56d0e9af218465636877cf8f7f
GERALD Jr <moswaldgerald@gmail.com>
2020-06-30 22:08:01 +0300

    routing

--- src/app/servers/server/server.component.ts
+++ src/app/servers/server/server.component.ts
@@ -14,11 +14,11 @@ export class ServerComponent implements OnInit {
   constructor(private serversService: ServersService, private route: ActivatedRoute) { }
 
   ngOnInit() {
-    const id = this.route.snapshot.params['id'];
+    const id = +this.route.snapshot.params['id'];
     this.server = this.serversService.getServer(id);
     this.route.params.subscribe(
       (params: Params)=>{
-        this.server = this.serversService.getServer(params['id']);
+        this.server = this.serversService.getServer(+params['id']);
       }
     )
   }
