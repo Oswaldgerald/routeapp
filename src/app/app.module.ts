8bbe1f134c3239b8d9dc906e5628a8e2c27f3565
GERALD Jr <moswaldgerald@gmail.com>
2020-07-10 00:11:50 +0300

    routing guard

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -13,6 +13,8 @@ import {ServerComponent} from './servers/server/server.component';
 import {ServersService} from './servers/servers.service';
 import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
 import {AppRoutingModule} from "./app-routing.module";
+import {AuthGuard} from "./auth.guard.service";
+import {AuthService} from "./auth.service";
 
 
 
@@ -33,7 +35,7 @@ import {AppRoutingModule} from "./app-routing.module";
     AppRoutingModule
 
   ],
-  providers: [ServersService],
+  providers: [ServersService, AuthGuard, AuthService],
   bootstrap: [AppComponent]
 })
 export class AppModule {
